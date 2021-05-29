33818575bc11c1c6550911321720539ffd832593
Oleksandr Zoria <zoriaalexandr@gmail.com>
2021-05-29 15:52:19 +0300

    refactored mediator

--- src/base/form-components.js
+++ src/base/form-components.js
@@ -2,42 +2,36 @@ function Input (node, options = { required: true, minLength: 5 }) {
   if (!node) throw new Error('Dom element should be provided')
 
   this.options = options
-  this.data = ''
 
   node.addEventListener('input', (e) => {
-    this.data = e.target.value
+    options.mediator.notify(e.target.value, this.options.fieldName)
   })
 
-  const errorMessage = () => {
+  const errorMessage = (msg) => {
     const existingErrorMessage = node.nextSibling
     if (existingErrorMessage) {
       existingErrorMessage.remove()
     }
 
     const div = document.createElement('div')
-    div.innerHTML = this.options.errorMessage
+    div.innerHTML = msg
     div.classList.add('error-message')
 
     node.parentNode.insertBefore(div, node.nextSibling)
   }
 
-  const validate = () => {
-    if (!this.options.required) return
+  this.validate = (options) => {
+    if (!options.required) return
     node.classList.remove('invalid')
 
-    if (this.data.length < this.options.minLength) {
+    if (!options.data || options.data.length < options.minLength) {
       node.classList.add('invalid')
-      errorMessage()
+      errorMessage(options.errorMessage)
       return false
     }
 
     node.nextSibling.remove()
     return true
-  }
-
-  this.notify = function () {
-    const isValid = validate()
-    if (isValid) return this.data
   }
 }
 
--- src/register/register.js
+++ src/register/register.js
@@ -3,129 +3,67 @@ import './index.scss'
 import { axios } from '../utils/axios'
 import { Input } from '../base/form-components'
 
-/*function FormHandler () {
-  const formData = {
-    userType: 'user'
-  }
-
-  let nodes = null
-
-  const selectors = function () {
-    const nameInput = document.getElementById('name')
-    const emailInput = document.getElementById('email')
-    const passwordInput = document.getElementById('password')
-    const checkBox = document.getElementById('check')
-    const dangerAlert = document.getElementById('danger')
-    const successAlert = document.getElementById('success')
-    const requiredFields = document.querySelectorAll('[data-required="required"]')
-    const button = document.querySelector('button[type="submit"]')
-
-    return {
-      nameInput,
-      emailInput,
-      passwordInput,
-      checkBox,
-      dangerAlert,
-      successAlert,
-      requiredFields,
-      button
-    }
-  }
-
-  const validateForm = function () {
-    let isValid = true
-
-    nodes.requiredFields.forEach(el => {
-      const { length, required } = el.dataset
-      el.classList.remove('invalid')
-
-      if (required && length > el.value.length) {
-        el.classList.add('invalid')
-        isValid = false
-      }
-
-      if (el.id === 'email' && !el.value.length) {
-        el.classList.add('invalid')
-        isValid = false
-      }
-    })
-
-    return isValid
-  }
-
-  const init = function () {
-    nodes = selectors()
-
-    const eventTarget = {
-      name: 'name',
-      email: 'email',
-      password: 'password'
-    }
-
-    for (const node in nodes) {
-      if (node && eventTarget[nodes[node].id]) {
-        nodes[node].addEventListener('input', e => {
-          formData[eventTarget[nodes[node].id]] = e.target.value
-        })
-      }
-    }
-
-    nodes.checkBox.addEventListener('change', e => {
-      const isChecked = nodes.checkBox.checked
-
-      if (isChecked) {
-        formData.userType = 'psychologist'
-      } else {
-        formData.userType = 'user'
-      }
-    })
-  }
-
-  init()
-
-  this.submitForm = function () {
-    nodes.dangerAlert.classList.add('hidden')
-    nodes.successAlert.classList.add('hidden')
-
-    if (validateForm()) {
-      nodes.button.disabled = true
-      axios.post('auth/register', formData)
-        .then(() => {
-          nodes.successAlert.classList.remove('hidden')
-          nodes.successAlert.innerText = 'U have been successfully registered'
-          nodes.nameInput.value = ''
-        })
-        .catch((e) => {
-          nodes.dangerAlert.innerText = e.response.data.message
-          nodes.dangerAlert.classList.remove('hidden')
-        })
-        .finally(() => nodes.button.disabled = false)
-    }
-  }
-}
-
-const form = document.getElementById('form')
-const handler = new FormHandler()
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault()
-  handler.submitForm()
-})*/
-
 function RegisterHandler () {
   const nameInput = document.getElementById('name')
   const form = document.getElementById('form')
+  const emailInput = document.getElementById('email')
+  const passwordInput = document.getElementById('password')
+
+  this.formData = {}
 
   const name = new Input(nameInput, {
-    required: true,
-    minLength: 4,
-    errorMessage: 'Name is required'
+    fieldName: 'name',
+    mediator: this
   })
+
+  const email = new Input(emailInput, {
+    fieldName: 'email',
+    mediator: this
+  })
+
+  const password = new Input(passwordInput, {
+    fieldName: 'password',
+    mediator: this
+  })
+
+  this.notify = function (data, fieldName) {
+    this.formData[fieldName] = data
+  }
+
+  const validate = () => {
+    const isNameValid = name.validate({
+      data: this.formData.name,
+      required: true,
+      minLength: 4,
+      errorMessage: 'Name is required'
+    })
+
+    const isEmailValid = email.validate({
+      data: this.formData.email,
+      required: true,
+      minLength: 4,
+      errorMessage: 'Email is required'
+    })
+
+    const isPasswordValid = password.validate({
+      data: this.formData.password,
+      required: true,
+      minLength: 4,
+      errorMessage: 'Password is required'
+    })
+
+    return isNameValid || isEmailValid || isPasswordValid;
+  }
 
   form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const nameData = name.notify()
-    console.log(nameData);
+    // const nameData = name.notify()
+    const isValid = validate()
+    if (isValid) {
+      alert('Valid')
+    } else {
+      alert('Not valid')
+    }
   })
 }
 
